e29f19fabd2f4b0b3495ed0ce6e500f94ce30275
Harshi <100234558+it21328152@users.noreply.github.com>
2022-02-27 11:55:59 +0000

    Tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -2,10 +2,21 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
-
-int main() {
-  
-  return 0;
+#include<iostream>
+int main()
+{
+	float cmlen,inchlen;
+	
+	  std::"cout<<Enter length in cm:";
+	  std::cin>>cmlen;
+	  
+	  std::cout<<std::std::endl;
+	  inchlen=cmlen/2.54;
+	  std::cout<<"Lenth in inches :"<<inchlen <<
+	  std::endl;
+	  
+	  return 0;
 }
-
+	  
+		
+}
