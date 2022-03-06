b637df274777c915ebe49b39c7d1b4040da71d51
Harshi <100234558+it21328152@users.noreply.github.com>
2022-03-06 20:10:46 +0000

    Tutorial 04 is completed

--- Tute04.c
+++ Tute04.c
@@ -6,6 +6,12 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+
+//function declaration
+int minimum(int no1 , int no2);
+int maximum(int no1 , int no2);
+int multiply(int no1 , int no2);
+
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
@@ -17,3 +23,43 @@ int main() {
    printf("%d ", multiply(no1, no2));
    return 0;
 }
+
+
+//function implementation
+int minimum(int no1 , int no2)  //function to find minimum
+{
+int a;
+
+  if(no1<no2)
+  {
+    return no1;
+
+  }
+  else
+  {
+    return no2;
+  }
+
+  }
+
+  int maximum(int no1 , int no2)  //function to find maximum
+  {
+  int b;
+
+  if(no1>no2)
+  {
+  return no1;
+  }
+  else
+  {
+    
+   return no2;
+  }
+
+
+  }
+
+  int multiply(int no1 , int no2)  //function to find multiplication
+  {
+    return no1 * no2;
+  }
