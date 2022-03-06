6e4184f141cc512cc74d1863a65d8db5ad4df1c2
Harshi <100234558+it21328152@users.noreply.github.com>
2022-03-06 18:39:02 +0000

    Tutorial 01 is completed

--- Tute01.c
+++ Tute01.c
@@ -1,22 +1,25 @@
 /* Exercise 1 - Calculations
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+#include <stdio.h>
 
-#include<iostream>
 int main()
 {
-	float cmlen,inchlen;
-	
-	  std::"cout<<Enter length in cm:";
-	  std::cin>>cmlen;
-	  
-	  std::cout<<std::std::endl;
-	  inchlen=cmlen/2.54;
-	  std::cout<<"Lenth in inches :"<<inchlen <<
-	  std::endl;
-	  
-	  return 0;
-}
-	  
-		
+    float mark1,mark2; 
+    float total, average;
+
+    /* Input marks of all five subjects */
+    printf("Enter marks of two subjects: \n");
+    scanf("%f%f", &mark1, &mark2);
+
+    /* Calculate total, average  */
+    total = mark1 + mark2 ;
+    average = total / 2.0;
+    
+
+    /* Print all results */
+    printf("Total marks = %.2f\n", total);
+    printf("Average marks = %.2f\n", average);
+    
+    return 0;
 }
